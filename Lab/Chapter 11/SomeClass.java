55ee833e0929c1e23b9fc084424af082ba6c14a2
Tony Pelaez <tnyplz@gmail.com>
2011-04-14 22:45:52 -0700

    added lab question a

---
+++ Lab/Chapter 11/SomeClass.java
@@ -0,0 +1,16 @@
+/*
+
+  Tony Pelaez
+  CSIT 142
+  Chapter 11
+
+  LAB HOMEWORK
+  ===========================================================================
+
+  (a) 11.20 Write a program that shows a constructor passing information 
+  about constructor failure to an exception handler.  Define class SomeClass,
+  which throws an Exception in the constructor.  Your program should try to 
+  create an object of type SomeClass and catch the exception that is thrown
+  from the constructor.
+
+*/
