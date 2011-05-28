b566ff11d607ae3be4c0db7ab4cbc6e8c586984a
Tony Pelaez <tnyplz@gmail.com>
2011-05-28 12:34:23 -0700

    added output stream

--- Final/ChatClient.java
+++ Final/ChatClient.java
@@ -3,10 +3,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.net.ConnectException;
+import java.net.InetAddress;
 import java.net.Socket;
 import javax.swing.Box;
 import javax.swing.JButton;
@@ -18,10 +22,13 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ChatClient extends JFrame implements ComponentListener{
-  private JTextField messageField; // field where user types message
-  private JTextArea  chatArea; // textarea where all messages are printed
-  private JButton    sendMessageButton; 
-  private JButton    quitButton;
+  private JTextField     messageField; // field where user types message
+  private JTextArea      chatArea; // textarea where all messages are printed
+  private JButton        sendMessageButton; 
+  private JButton        quitButton;
+  private Socket         client;
+  private PrintStream    output;
+  private BufferedReader input;
   
   // Default width and height
   // if the user decreases the size of the window, it will resize to the 
@@ -52,21 +59,20 @@ public class ChatClient extends JFrame implements ComponentListener{
     textFieldBox.add( messageField );
 
     sendMessageButton = new JButton("Send");
-    sendMessageButton.addActionListener(
-      new ActionListener(){
-        public void actionPerformed(ActionEvent e){
-          //Send message
-          messageField.setText("");
-        }
-      }
-    );
+    sendMessageButton.addActionListener(new SendListener());
     buttonBox.add(sendMessageButton);
 
     quitButton = new JButton("Quit");
     quitButton.addActionListener(
       new ActionListener(){
         public void actionPerformed(ActionEvent e){
-          System.exit(0);
+          try {
+            output.close();
+            client.close();
+            System.exit(0);
+          } catch (IOException ioException) {
+            ioException.printStackTrace();
+          }
         }
       }
     );
@@ -74,17 +80,29 @@ public class ChatClient extends JFrame implements ComponentListener{
 
     add(textFieldBox, BorderLayout.CENTER);
     add(buttonBox, BorderLayout.EAST);   
+
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
   }
 
-  public void launchFrame(){
-    //make the GUI
-    ChatClient window = new ChatClient();
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  public void connect(){
     //eastablish the socket connection
-    //make a thread and start it with an instance of RemoteReader
-    //create the streams from the socket connection
-    //make gui visible
-    window.setVisible(true);
+    try {
+      client = new Socket("127.0.0.1", 2000);
+      if (client == null) {
+        throw new IOException();
+      }
+
+      //make a thread and start it with an instance of RemoteReader
+
+      //create the streams from the socket connection
+      output = new PrintStream(client.getOutputStream());
+
+      input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
    
   public class SendListener implements ActionListener{
@@ -92,7 +110,13 @@ public class ChatClient extends JFrame implements ComponentListener{
     {
       //get info from text field
       //send it to the server
+      String msg = messageField.getText() + "\n";
+      output.println(msg);
+      chatArea.append(msg);
+      output.flush();
+
       //clear the text field
+      messageField.setText("");
     }
   }
     
@@ -128,7 +152,7 @@ public class ChatClient extends JFrame implements ComponentListener{
 
   public static void main(String args[]) {
     ChatClient chatWindow = new ChatClient();
-    chatWindow.launchFrame();
+    chatWindow.connect();
 
     // try {
     //   // Open your connection to a server, at port 5432
